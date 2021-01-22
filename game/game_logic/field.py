2e77e3b168f349bb7b020cca8f1268692396094a
Christa Bridges <christa@pebblepad.co.uk>
2021-01-22 19:35:09 +0000

    code-comments Comments added on field.py

--- game/game_logic/field.py
+++ game/game_logic/field.py
@@ -17,6 +17,7 @@ def enter_field(player):
     monster = Monster()
     playing = True
 
+    # Loops until the player leaves the field
     while playing:
         # Monster fight
         text_delay(f"You have encountered a {monster.name}")
@@ -35,26 +36,35 @@ def enter_field(player):
             else:
                 coins_earned = 5
 
+            # Update the player object and JSON
             player.coins += coins_earned
             FileLogic().update_player_property(PLAYER_FILENAME, player, "Coins", player.coins)
             text_delay(f"You earned {coins_earned} coins putting your total to {player.coins}.")
 
+            # Updates the player's record of the monsters they have killed
             if monster.name not in player.monsters_killed:
+                # Set value of monster if it is the first on the player has killed of this species
                 player.monsters_killed[monster.name] = 1
             else:
+                # Append the number killed if the player has killed this species before
                 player.monsters_killed[monster.name] += 1
 
             FileLogic().update_player_property(PLAYER_FILENAME, player, "Monsters Killed", player.monsters_killed)
 
         # If the player dies.
         if player.health <= 0:
+            # Set health to 0 so it is not a negative number
             player.health = 0
             print("------------------------------------------------------------------------------")
             text_delay("You have died losing all coins and items on your person.")
+            # Removes all items from their inventory and coins, and updates the JSON.
             player.inventory = {}
             player.coins = 0
             FileLogic().update_player_property(PLAYER_FILENAME, player, "Coins", player.coins)
             FileLogic().update_player_property(PLAYER_FILENAME, player, "Inventory", player.inventory)
+            # Calls the medical centre method to restore health
             text_delay("You've been transported to the medical centre for emergency help!")
             MedicalCentre(player).enter_medical_centre(True)
+
+        # Stop the loop
         playing = False
